882bb0f72cabc375ffe3eabffa6485df50ae0157
Max Morrison <maxrmorrison@gmail.com>
2019-05-21 13:18:46 -0500

    Merged load_mnist into load_data

--- code/load_data.py
+++ code/load_data.py
@@ -1,7 +1,8 @@
 import json
 import numpy as np
 import os
-from code import load_mnist
+import struct
+from array import array as pyarray
 
 
 def load_data(dataset, fraction=1.0):
@@ -123,6 +124,111 @@ def load_mnist_data(threshold, fraction=1.0, examples_per_class=500):
     test_features = test_features.reshape((len(test_features), -1))
 
     return train_features, test_features, train_targets, test_targets
+
+
+def load_mnist(path, dataset="training", digits=None, asbytes=False,
+               selection=None, return_labels=True, return_indices=False):
+    """
+    Loads MNIST files into a 3D numpy array. Does not automatically download
+    the dataset. You must download the dataset manually. The data can be
+    downloaded from http://yann.lecun.com/exdb/mnist/.
+
+    Examples:
+        1) Assuming that you have downloaded the MNIST database in a directory
+        called 'data', this will load all images and labels from the training
+        set:
+
+            images, labels = load_mnist('training')
+
+        2) And this will load 100 sevens from the test partition:
+
+            sevens = load_mnist('testing', digits=[7], selection=slice(0, 100),
+                                return_labels=False)
+
+    Arguments:
+        path - (str) Path to your MNIST datafiles.
+        dataset - (str) Either "training" or "testing". The data partition to
+            load.
+        digits - (list or None) A list of integers specifying the digits to
+            load. If None, the entire database is loaded.
+        asbytes - (bool) If True, returns data as ``numpy.uint8`` in [0, 255]
+            as opposed to ``numpy.float64`` in [0.0, 1.0].
+        selection - (slice) Using a `slice` object, specify what subset of the
+            dataset to load. An example is ``slice(0, 20, 2)``, which would
+            load every other digit until--but not including--the twentieth.
+        return_labels - (bool) Specify whether or not labels should be
+            returned. This is also a speed performance if digits are not
+            specified, since then the labels file does not need to be read at
+            all.
+        return_indicies - (bool) Specify whether or not to return the MNIST
+            indices that were fetched. This is valuable only if digits is
+            specified, because in that case it can be valuable to know how far
+            in the database it reached.
+    Returns:
+        images - (np.array) Image data of shape ``(N, rows, cols)``, where
+            ``N`` is the number of images. If neither labels nor indices are
+            returned, then this is returned directly, and not inside a 1-sized
+            tuple.
+        labels - (np.array) Array of size ``N`` describing the labels.
+            Returned only if ``return_labels`` is `True`, which is default.
+        indices - (np.array) The indices in the database that were returned.
+    """
+
+    # The files are assumed to have these names and should be found in 'path'
+    files = {
+        'training': ('train-images-idx3-ubyte', 'train-labels-idx1-ubyte'),
+        'testing': ('t10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte'),
+    }
+
+    try:
+        images_fname = os.path.join(path, files[dataset][0])
+        labels_fname = os.path.join(path, files[dataset][1])
+    except KeyError:
+        raise ValueError("Data set must be 'testing' or 'training'")
+
+    # We can skip the labels file only if digits aren't specified and labels
+    # aren't asked for
+    if return_labels or digits is not None:
+        flbl = open(labels_fname, 'rb')
+        magic_nr, size = struct.unpack(">II", flbl.read(8))
+        labels_raw = pyarray("b", flbl.read())
+        flbl.close()
+
+    fimg = open(images_fname, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    images_raw = pyarray("B", fimg.read())
+    fimg.close()
+
+    if digits:
+        indices = [k for k in range(size) if labels_raw[k] in digits]
+    else:
+        indices = range(size)
+
+    if selection:
+        indices = indices[selection]
+
+    images = np.zeros((len(indices), rows, cols), dtype=np.uint8)
+
+    if return_labels:
+        labels = np.zeros((len(indices)), dtype=np.int8)
+    for i in range(len(indices)):
+        images[i] = np.array(images_raw[indices[i] * rows * cols:(indices[i] + 1) * rows * cols]).reshape((rows, cols))
+        if return_labels:
+            labels[i] = labels_raw[indices[i]]
+
+    if not asbytes:
+        images = images.astype(float)/255.0
+
+    ret = (images,)
+    if return_labels:
+        ret += (labels,)
+    if return_indices:
+        ret += (indices,)
+
+    if len(ret) == 1:
+        return ret[0]  # Don't return a tuple of one
+
+    return ret
 
 
 def stratified_subset(features, targets, examples_per_class):
